5f8e438bb0c49d13ef6033d9684968ad9ccddad8
vitaxa <vitaxa93gamebox@gmail.com>
2017-04-13 23:34:47 +0300

    Cleanup

--- src/main/java/com/vitaxa/steamauth/helper/IOHelper.java
+++ src/main/java/com/vitaxa/steamauth/helper/IOHelper.java
@@ -1,58 +1,15 @@
 package com.vitaxa.steamauth.helper;
 
-import java.awt.*;
 import java.io.*;
-import java.net.*;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.attribute.FileTime;
-import java.nio.file.attribute.PosixFilePermission;
-import java.util.Collections;
-import java.util.Set;
-import java.util.UUID;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
-import java.awt.*;
-import java.io.*;
-import java.net.*;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
-import java.nio.file.FileSystem;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.attribute.FileTime;
-import java.nio.file.attribute.PosixFilePermission;
-import java.util.Collections;
-import java.util.Set;
-import java.util.UUID;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
-import static java.nio.file.FileVisitResult.CONTINUE;
 
 public final class IOHelper {
     // Charset
     public static final Charset UNICODE_CHARSET = StandardCharsets.UTF_8;
-    public static final Charset ASCII_CHARSET = StandardCharsets.US_ASCII;
-
-    // Constants
-    public static final int BUFFER_SIZE = Integer.parseUnsignedInt(System.getProperty("jasteam.bufferSize",
-            Integer.toString(4096)));
-
-    // Paths
-    public static final Path JVM_DIR = Paths.get(System.getProperty("java.home"));
-    public static final Path WORKING_DIR = Paths.get(System.getProperty("user.dir"));
-    public static final Path HOME_DIR = Paths.get(System.getProperty("user.home"));
-    public static final Path TMP_DIR = Paths.get(System.getProperty("java.io.tmpdir"));
-
-    // Platform-dependent
-    public static final String CROSS_SEPARATOR = "/";
-    public static final FileSystem FS = FileSystems.getDefault();
-    public static final String PLATFORM_SEPARATOR = FS.getSeparator();
-    public static final String OS = System.getProperty("os.name");
 
     // File options
     private static final LinkOption[] LINK_OPTIONS = {};
@@ -60,7 +17,6 @@ public final class IOHelper {
     private static final CopyOption[] COPY_OPTIONS = { StandardCopyOption.REPLACE_EXISTING };
     private static final OpenOption[] APPEND_OPTIONS = { StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND };
     private static final OpenOption[] WRITE_OPTIONS = { StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING };
-    private static final Set<FileVisitOption> WALK_OPTIONS = Collections.singleton(FileVisitOption.FOLLOW_LINKS);
 
     private IOHelper() {
     }
@@ -72,20 +28,6 @@ public final class IOHelper {
             //LogHelper.error(exc);
         }
     }
-
-
-    public static void copy(Path source, Path target) throws IOException {
-        createParentDirs(target);
-        Files.copy(source, target, COPY_OPTIONS);
-    }
-
-    public static void copy(String source, String target) throws IOException {
-        Path dest = Paths.get(target);
-        Path src = Paths.get(source);
-        createParentDirs(dest);
-        Files.copy(src, dest, COPY_OPTIONS);
-    }
-
 
     public static void createParentDirs(Path path) throws IOException {
         Path parent = path.getParent();
@@ -104,54 +46,13 @@ public final class IOHelper {
         return new String(bytes, UNICODE_CHARSET);
     }
 
-
-    public static String decodeASCII(byte[] bytes) {
-        return new String(bytes, ASCII_CHARSET);
-    }
-
-
-    public static void deleteDir(Path dir, boolean self) throws IOException {
-        walk(dir, new DeleteDirVisitor(dir, self));
-    }
-
     public static byte[] encode(String s) {
         return s.getBytes(UNICODE_CHARSET);
     }
 
-
-    public static byte[] encodeASCII(String s) {
-        return s.getBytes(ASCII_CHARSET);
-    }
-
-
     public static boolean exists(Path path) {
         return Files.exists(path, LINK_OPTIONS);
     }
-
-    public static String getFileName(Path path) {
-        return path.getFileName().toString();
-    }
-
-    public static byte[] getResourceBytes(String name) throws IOException {
-        return read(getResourceURL(name));
-    }
-
-    public static Path getCodeSource(Class<?> clazz) {
-        return Paths.get(toURI(clazz.getProtectionDomain().getCodeSource().getLocation()));
-    }
-
-    public static URL getResourceURL(String name) throws NoSuchFileException {
-        URL url = ClassLoader.getSystemResource(name);
-        if (url == null) {
-            throw new NoSuchFileException(name);
-        }
-        return url;
-    }
-
-    public static boolean hasExtension(Path file, String extension) {
-        return getFileName(file).endsWith('.' + extension);
-    }
-
 
     public static boolean isDir(Path path) {
         return Files.isDirectory(path, LINK_OPTIONS);
@@ -167,22 +68,6 @@ public final class IOHelper {
 
     public static boolean isFile(Path path) {
         return Files.isRegularFile(path, LINK_OPTIONS);
-    }
-
-
-    public static boolean isValidFileName(String fileName) {
-        return !fileName.equals(".") && !fileName.equals("..") &&
-                fileName.chars().noneMatch(ch -> ch == '/' || ch == '\\') && isValidPath(fileName);
-    }
-
-
-    public static boolean isValidPath(String path) {
-        try {
-            toPath(path);
-            return true;
-        } catch (InvalidPathException ignored) {
-            return false;
-        }
     }
 
     public static void move(Path source, Path target) throws IOException {
@@ -267,44 +152,11 @@ public final class IOHelper {
         return newWriter(new FileOutputStream(fd));
     }
 
-    public static ZipEntry newZipEntry(String name) {
-        ZipEntry entry = new ZipEntry(name);
-        entry.setTime(0);
-        return entry;
-    }
-
-
-    public static ZipEntry newZipEntry(ZipEntry entry) {
-        return newZipEntry(entry.getName());
-    }
-
-    public static ZipOutputStream newZipOutput(FileOutputStream output) {
-        return new ZipOutputStream(output);
-    }
-
-    public static byte[] read(Path file) throws IOException {
-        long size = readAttributes(file).size();
-        if (size > Integer.MAX_VALUE) {
-            throw new IOException("File too big");
-        }
-
-        // Read bytes from file
-        byte[] bytes = new byte[(int) size];
-        try (InputStream input = newInput(file)) {
-            read(input, bytes);
-        }
-
-        // Return result
-        return bytes;
-    }
-
-
     public static byte[] read(URL url) throws IOException {
         try (InputStream input = newInput(url)) {
             return read(input);
         }
     }
-
 
     public static void read(InputStream input, byte[] bytes) throws IOException {
         int offset = 0;
@@ -338,65 +190,6 @@ public final class IOHelper {
         return response;
     }
 
-
-    public static BasicFileAttributes readAttributes(Path path) throws IOException {
-        return Files.readAttributes(path, BasicFileAttributes.class, LINK_OPTIONS);
-    }
-
-    public static String readLine() {
-        String s = "";
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            s = br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return s;
-    }
-
-    public static Path toPath(String path) {
-        return Paths.get(path.replace(PLATFORM_SEPARATOR, CROSS_SEPARATOR));
-    }
-
-    public static String toString(Path path) {
-        return path.toString().replace(PLATFORM_SEPARATOR, CROSS_SEPARATOR);
-    }
-
-    public static URI toURI(URL url) {
-        try {
-            return url.toURI();
-        } catch (URISyntaxException e) {
-            throw new IllegalArgumentException(e);
-        }
-    }
-
-    public static String removeExtension(String s) {
-        String separator = System.getProperty("file.separator");
-        String filename;
-
-        int lastSeparatorIndex = s.lastIndexOf(separator);
-        if (lastSeparatorIndex == -1) {
-            filename = s;
-        } else {
-            filename = s.substring(lastSeparatorIndex + 1);
-        }
-
-        // Remove the extension.
-        int extensionIndex = filename.lastIndexOf(".");
-        if (extensionIndex == -1) {
-            return filename;
-        }
-        return filename.substring(0, extensionIndex);
-    }
-
-    public static void setAttribute(Path file, String attribute, String value) throws IOException {
-        Files.setAttribute(file, attribute, value, LINK_OPTIONS);
-    }
-
-    public static void setAttribute(Path file, String attribute, FileTime time) throws IOException {
-        Files.setAttribute(file, attribute, time, LINK_OPTIONS);
-    }
-
     public static int transfer(InputStream input, OutputStream output) throws IOException {
         int transferred = 0;
         byte[] buffer = newBuffer();
@@ -426,43 +219,9 @@ public final class IOHelper {
         }
     }
 
-    public static void walk(Path dir, FileVisitor<Path> visitor) throws IOException {
-        Files.walkFileTree(dir, WALK_OPTIONS, Integer.MAX_VALUE, visitor);
-    }
-
     public static void write(Path file, byte[] bytes) throws IOException {
         createParentDirs(file);
         Files.write(file, bytes, WRITE_OPTIONS);
-    }
-
-    public static void writeAppend(Path file, byte[] bytes) throws IOException {
-        createParentDirs(file);
-        Files.write(file, bytes, APPEND_OPTIONS);
-    }
-
-    private static final class DeleteDirVisitor extends SimpleFileVisitor<Path> {
-        private final Path dir;
-        private final boolean self;
-
-        private DeleteDirVisitor(Path dir, boolean self) {
-            this.dir = dir;
-            this.self = self;
-        }
-
-        @Override
-        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-            FileVisitResult result = super.postVisitDirectory(dir, exc);
-            if (self || !this.dir.equals(dir)) {
-                Files.delete(dir);
-            }
-            return result;
-        }
-
-        @Override
-        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-            Files.delete(file);
-            return super.visitFile(file, attrs);
-        }
     }
 }
 
